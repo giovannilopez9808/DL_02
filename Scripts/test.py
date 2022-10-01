bee7d6e732985b4f72bb95bc08807a15eddfd4cc
Giovanni Lopez <giovannilopez9808@gmail.com>
2022-10-01 17:29:19 -0500

    Update

--- Scripts/test.py
+++ Scripts/test.py
@@ -1,75 +1,50 @@
-from Modules.pix2pix import VAE_pix2pix_model
+from Modules.VAE_pix2pix import VAE_pix2pix_model
 from Modules.dataset import dataset_model
 from Modules.params import get_params
 import matplotlib.pyplot as plt
-from numpy.random import normal
-from numpy import (
-    linspace,
-    arange,
-    round,
-    zeros,
-)
+from os.path import join
+from numpy import array
 
 
-def plot_latent_space(model: VAE_pix2pix_model,
-                      input_size: tuple = (28, 28, 1),
-                      n: int = 30,
-                      figsize: int = 15,
-                      scale: float = 1.,
-                      latents_start: list = [0, 1]):
-    # display a n*n 2D manifold of digits
-    canvas = zeros((input_size[0]*n,
-                    input_size[1]*n,
-                    input_size[2]))
-    # linearly spaced coordinates corresponding to the 2D plot
-    # of digit classes in the latent space
-    grid_x = linspace(-scale,
-                      scale,
-                      n)
-    grid_y = linspace(-scale,
-                      scale,
-                      n)[::-1]
-    z_sample = normal(0, 1,
-                      (1, model.vae_x.latent_dim))
-    for i, yi in enumerate(grid_y):
-        for j, xi in enumerate(grid_x):
-            z_sample[0][latents_start[0]] = xi
-            z_sample[0][latents_start[1]] = yi
-            x_decoded = model.vae_x.decoder_model(z_sample)
-            img = x_decoded[0].numpy().reshape(input_size)
-            pos_x_i = i*input_size[0]
-            pos_x_j = (i+1)*input_size[0]
-            pos_y_i = j*input_size[1]
-            pos_y_j = (j+1)*input_size[1]
-            print(img)
-            canvas[pos_x_i: pos_x_j, pos_y_i: pos_y_j, :] = img
-
-    plt.figure(figsize=(figsize, figsize))
-    start_range = input_size[0] // 2
-    end_range = n*input_size[0] + start_range
-    pixel_range = arange(start_range,
-                         end_range,
-                         input_size[0])
-    sample_range_x = round(grid_x,
-                           1)
-    sample_range_y = round(grid_y,
-                           1)
-    plt.xticks(pixel_range,
-               sample_range_x)
-    plt.yticks(pixel_range,
-               sample_range_y)
-    plt.xlabel(f"z[{latents_start[0]}]")
-    plt.ylabel(f"z[{latents_start[1]}]")
-    plt.imshow(canvas,
-               cmap="Greys_r")
-    plt.savefig("test.png")
+def plot_image(ax: plt.subplot,
+               image: array,
+               label: str) -> None:
+    image = image[0]
+    image = (image+1)/2
+    ax.set_title(label)
+    ax.imshow(image)
+    ax.axis("off")
 
 
 params = get_params()
+params["dataset"]["type"] = "all"
 dataset = dataset_model(params)
-model = VAE_pix2pix_model(**params["VAE"])
-plot_latent_space(model,
-                  input_size=params["VAE"]["input_dim"],
-                  n=6,
-                  latents_start=[20, 30],
-                  scale=3)
+model = VAE_pix2pix_model(params)
+dog, cat = list(dataset.train.take(1))[0]
+pred_dog = model.vae_dog(dog)
+gen_cat = model.generator_cat(pred_dog)
+pred_cat = model.vae_cat(cat)
+gen_dog = model.generator_dog(pred_cat)
+fig, axs = plt.subplots(2, 3,
+                        figsize=(15, 10))
+axs = axs.flatten()
+plot_image(axs[0],
+           dog,
+           "photo")
+plot_image(axs[1],
+           pred_dog,
+           "decoder dog")
+plot_image(axs[2],
+           gen_cat,
+           "cat generate")
+plot_image(axs[3],
+           cat,
+           "cat")
+plot_image(axs[4],
+           pred_cat,
+           "decoder cat")
+plot_image(axs[5],
+           gen_dog,
+           "dog generate")
+plt.tight_layout(pad=2)
+plt.savefig("cycleGAN.png")
--- Scripts/train.py
+++ Scripts/train.py
@@ -1,11 +1,17 @@
+from Modules.VAE_pix2pix import VAE_pix2pix_model
 from Modules.dataset import dataset_model
-from Modules.model import CycleGAN_model
 from Modules.params import get_params
-# from Modules.model import model
+from pandas import DataFrame
+from os.path import join
 
 params = get_params()
+params["dataset"]["type"] = "all"
 dataset = dataset_model(params)
-model = CycleGAN_model(params)
-history=model.run(dataset.train)
-history.to_csv("../history.csv",
+model = VAE_pix2pix_model(params)
+print(params["epochs"])
+history = model.fit(dataset.train,
+                    epochs=params["epochs"])
+history = DataFrame(history)
+filename = "../cycleGAN_history.csv"
+history.to_csv(filename,
                index=False)
